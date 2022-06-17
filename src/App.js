345798245c268be09203a816d1f1269be8fb9bce
jdaih <justinzero2008@yahoo.com>
2022-06-17 03:41:02 -0500

    set-up component files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
